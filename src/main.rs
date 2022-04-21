d238d237f929a97c9715f7af04419ece57840d09
nguyenvukhang <brew4k@gmail.com>
2022-04-22 00:36:21 +0800

    can take in command output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use std::process::Command;
+use std::io::{self, Write};
 
 fn main() {
-    println!("Gonna git good")
+    println!("Gonna git good");
+    let mut git = Command::new("git");
+    let status = git.arg("status");
+    let hello = status.output().expect("failed to execute command.");
+    println!("Command output: {}", hello.status);
+    io::stdout().write_all(&hello.stdout).unwrap();
+    io::stderr().write_all(&hello.stderr).unwrap();
 }
