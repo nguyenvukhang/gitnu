414f01e25efc0d380fb0ca1fc7ec35fc2479c502
nguyenvukhang <brew4k@gmail.com>
2022-04-22 17:09:07 +0800

    read filename of each file

--- src/main.rs
+++ src/main.rs
@@ -35,18 +35,37 @@ fn main() {
     // let lines: Vec<String> = string.lines()
     let vec = string.lines();
 
-    // let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
-    let staged_start_regex = Regex::new(r"^Changes to be committed:.*").unwrap();
+    let staged_start_regex = Regex::new(r"^Changes to be committed:$").unwrap();
+    let unstaged_start_regex = Regex::new(r"^Changes not staged for commit:$").unwrap();
+    let untracked_start_regex = Regex::new(r"^Untracked files:$").unwrap();
     let mut reading_staged = false;
-    // let staged_index = 0;
-    // let staged_changes: [&str; 100] = [""; 100];
+    let mut reading_unstaged = false;
+    let mut reading_untracked = false;
+    let mut mi = 0;
+    let mut numbered_changes: [(&str, &str); 100] = [("", ""); 100];
 
     for i in vec {
         if i == "" {
             reading_staged = false;
+            reading_unstaged = false;
+            reading_untracked = false;
         }
         if reading_staged {
-            println!("read-staged: {}", i);
+            println!("staged: {}", i);
+            numbered_changes[mi] = ("staged", i);
+            mi += 1;
+            continue;
+        };
+        if reading_unstaged {
+            println!("unstaged: {}", i);
+            numbered_changes[mi] = ("unstaged", i);
+            mi += 1;
+            continue;
+        };
+        if reading_untracked {
+            println!("untracked: {}", i);
+            numbered_changes[mi] = ("untracked", i);
+            mi += 1;
             continue;
         };
         if staged_start_regex.is_match(i) {
@@ -54,10 +73,52 @@ fn main() {
             reading_staged = true;
             continue;
         };
+        if unstaged_start_regex.is_match(i) {
+            println!("------------- Start reading unstaged changes");
+            reading_unstaged = true;
+            continue;
+        };
+        if untracked_start_regex.is_match(i) {
+            println!("------------- Start reading untracked files");
+            reading_untracked = true;
+            continue;
+        };
         println!("line: {}", i)
     }
 
     println!("====================");
+    println!("POST PROCESSING");
+
+    // fn splitter() {
+    //     let s = "KEY1=value1=thing1";
+    //     match s.split_once('=') {
+    //         Some((key, value)) => {
+    //             println!("key: {}", key);
+    //             println!("value: {}", value);
+    //         }
+    //         None => {
+    //             println!("expected a key-value pair");
+    //         }
+    //     }
+    // }
+
+    for i in 0..100 {
+        if numbered_changes[i].0 != "" {
+            let (t, s) = numbered_changes[i];
+            if t == "staged" || t == "unstaged" {
+                match s.split_once(":") {
+                    Some((_, value)) => {
+                        println!("filename: {}", value.trim());
+                    }
+                    None => {
+                        println!("expected line to have a colon");
+                    }
+                };
+                continue;
+            }
+            println!("{}: {}", i, s);
+        }
+    }
 
     // read the contents of gitnumber.txt and use it to do cool stuff
 }
