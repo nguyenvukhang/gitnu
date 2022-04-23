f05430d32335abf36c43a597c24d04979b815491
nguyenvukhang <brew4k@gmail.com>
2022-04-23 15:44:43 +0800

    factored out string sanitizing

---
+++ python/strings.py
@@ -0,0 +1,17 @@
+def remove_ansi(string: str) -> str:
+    ansi = [
+        "\x1b[31m",
+        "\x1b[32m",
+        "\x1b[33m",
+        "\x1b[34m",
+        "\x1b[35m",
+        "\x1b[36m",
+        "\x1b[37m",
+        "\x1b[m",
+    ]
+    for i in ansi:
+        string = string.replace(i, "")
+    return string
+
+def sanitize(string: str) -> str:
+    return remove_ansi(string.strip())
