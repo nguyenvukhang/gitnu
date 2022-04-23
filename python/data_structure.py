2b9ee6b0698439ba465c55a5d0339cd560143966
nguyenvukhang <brew4k@gmail.com>
2022-04-23 17:59:53 +0800

    remove old data structure

--- python/data_structure.py
+++ python/data_structure.py
@@ -11,12 +11,6 @@ class Entry:
         self.index = index
         self.line = line
 
-    def get_state(self):
-        return self.state
-
-    def get_action(self):
-        return self.action
-
     def get_filename(self):
         return self.filename
 
@@ -35,18 +29,6 @@ class Entry:
     def set_filename(self, value):
         self.filename = value
 
-    def set_index(self, value):
-        self.index = value
-
-    def set_line(self, value):
-        self.line = value
-
-    def tuple(self):
-        return (self.index, self.state, self.action, self.filename, self.line)
-
-    def list(self):
-        return [self.index, self.state, self.action, self.filename, self.line]
-
     def cache(self):
         return [self.index, self.filename]
 
@@ -54,9 +36,6 @@ class Entry:
 class NumberedStatus:
     def __init__(self) -> None:
         self.data: list[Entry] = []
-
-    def clear(self):
-        self.data = []
 
     def push(self, entry: Entry):
         self.data.append(entry)
@@ -74,11 +53,8 @@ class NumberedStatus:
             return self.data[i - 1].get_filename()
         return index
 
-    def get_data(self):
+    def copy(self):
         return tuple(self.data)
-
-    def get_tuples(self):
-        return list(map(lambda x: x.tuple(), self.data))
 
     def cache(self):
         return list(map(lambda x: x.cache(), self.data))
@@ -86,44 +62,18 @@ class NumberedStatus:
     def is_empty(self):
         return len(self.data) == 0
 
-    def print(self):
-        log.yellow(self.get_tuples())
-
     def length(self):
         return len(self.data)
 
-
-def make_entry(status, action, filename):
-    return [status, action, filename]
-
-
-def make_empty_entry():
-    return ["", "", ""]
-
-
-def get_status(elem):
-    return elem[0]
-
-
-def get_action(elem):
-    return elem[1]
-
-
-def get_filename(elem):
-    return elem[2]
-
-
-def add_delta(index, entry, table):
-    table[index] = entry
+    def print(self):
+        log.yellow(self.cache())
 
 
 def fill_table(numbered_status: NumberedStatus) -> None:
-    table = {}
     cwd = getcwd()
     state = "unset"  # 'staged' | 'unstaged' | 'untracked'
-    data_list = numbered_status.get_data()
+    data_list = numbered_status.copy()
     for data in data_list:
-        index = data.get_index()
         line = data.get_line()
         state = git.set_state.get(line, state)
         # these lines will not have filenames inside
@@ -142,15 +92,13 @@ def fill_table(numbered_status: NumberedStatus) -> None:
                 # too lazy to regex start-of-line,
                 # so this too shall pass
                 line = line[len(keyword) :]
-                if action == 'renamed':
-                    after_arrow = line.lstrip().split(' -> ', 1)[1]
+                if action == "renamed":
+                    after_arrow = line.lstrip().split(" -> ", 1)[1]
                     filename = path.join(cwd, after_arrow)
                 else:
                     filename = path.join(cwd, line.lstrip())
                 # use the first match only, so break after first if
                 break
-        entry = make_entry(state, action, filename)
         data.set_action(action)
         data.set_state(state)
         data.set_filename(filename)
-        add_delta(index, entry, table)
