3d25f008495cbbf3e5b8be909d3e99fc35936c7c
nguyenvukhang <brew4k@gmail.com>
2022-04-26 00:31:36 +0800

    c can read git status line by line

--- c/main.cpp
+++ c/main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
+#include <unistd.h>
 
 using namespace std;
 
+
+
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        cout << buffer.data();
+        result += buffer.data();
+        /* result += "hello"; */
+    }
+    return result;
+}
+
 int main() {
-  cout << "Hello World";
+  /* system("git status"); */
+  std::string output;
+  exec("git -c status.color=always status");
   return 0;
 }
+
