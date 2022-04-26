476db402f5e354beb7aba3b8a786050c93494de9
nguyenvukhang <brew4k@gmail.com>
2022-04-27 00:21:21 +0800

    done with benching

--- c/bench.js
+++ c/bench.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 
-const use = "CPP"
-
+const use = 'CPP'
 const fs = require('fs')
 const { exec } = require('child_process')
 const process = require('process')
 
-
-const commands = {
+const executables = {
   CPP: '../../dist/cgit',
   PYTHON: 'gitnu',
   GIT: 'git status',
@@ -22,16 +20,29 @@ function timed(command) {
 const original = process.cwd()
 process.chdir('repo/some/thing')
 const startTime = performance.now()
-timed(commands[use])
+timed(executables[use])
 const endTime = performance.now()
 const result = endTime - startTime
 console.debug('Elapsed time:', result)
 process.chdir(original)
 
-const content = use + ' : ' + result.toString() + '\n'
+const d = new Date()
+const D = d.toLocaleDateString()
+const T = d.toLocaleTimeString()
 
-fs.appendFile('time.txt', content, (err) => {
-  if (err) {
-    console.error(err)
+const data = JSON.parse(fs.readFileSync('time.json'))
+
+exec('git rev-parse --verify HEAD', (_, commitId) => {
+  const packet = {
+    use,
+    result,
+    timestamp: `${D} ${T}`,
+    commit: commitId.trimEnd(),
   }
+  data.push(packet)
+  fs.writeFile('time.json', JSON.stringify(data), (err) => {
+    if (err) {
+      console.error(err)
+    }
+  })
 })
