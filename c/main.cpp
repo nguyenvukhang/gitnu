9835141a8ec52951fdf4b915caf9858f35dbcf5a
nguyenvukhang <brew4k@gmail.com>
2022-04-26 10:35:50 +0800

    first line of shell output

--- c/main.cpp
+++ c/main.cpp
@@ -5,8 +5,6 @@
 #include <stdexcept>
 #include <string>
 
-using namespace std;
-
 bool printer(int index, char *line) {
   char red[6] = "\x1b[31m";
   char green[6] = "\x1b[32m";
@@ -14,30 +12,30 @@ bool printer(int index, char *line) {
   has_red = strstr(line, red);
   has_green = strstr(line, green);
   if (has_red || has_green) {
-    cout << index << line;
+    std::cout << index << line;
     index++;
     return true;
   } else {
-    cout << line;
+    std::cout << line;
     return false;
   }
 }
 
 // source:
 // https://stackoverflow.com/questions/478898/how-do-i-execute-a-command-and-get-the-output-of-the-command-within-c-using-po
-string exec(const char *cmd) {
+std::string gitnu_status(const char *cmd) {
   // and array of characters, length 128
-  array<char, 128> buffer;
-  string result;
+  std::array<char, 128> buffer;
+  std::string result;
   // no idea what this does
   // but if I sub pipe() with a variable name, it fails
   // pipe is a given name
   // FILE is probably a typedef struct
   // popen, pclose seem to be methods of either FILE or unique_ptr
-  unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
   // if no pipe, throw error: subprocess failed to start
   if (!pipe) {
-    throw runtime_error("popen() failed!");
+    throw std::runtime_error("popen() failed!");
   }
   int index = 1;
   bool had_filename;
@@ -52,7 +50,41 @@ string exec(const char *cmd) {
   return result;
 }
 
+namespace shell {
+/**
+ * first line of output of a shell command
+ * @param cmd the shell command to be run
+ * @return the first line of output as a string
+ */
+std::string line(const char *cmd) {
+  std::array<char, 128> buffer;
+  std::string result = "";
+  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+  bool written = false;
+  if (!pipe)
+    throw std::runtime_error("popen() failed!");
+  while (!feof(pipe.get()) && !written) {
+    if (fgets(buffer.data(), 128, pipe.get()) != nullptr) {
+      written = true;
+      result += buffer.data();
+    }
+  }
+  int rc = pclose(pipe.get());
+  if (rc == EXIT_SUCCESS) {        // == 0
+  } else if (rc == EXIT_FAILURE) { // EXIT_FAILURE is not used by all programs,
+  }
+  return result;
+}
+} // namespace shell
+
+namespace gitnu {} // namespace gitnu
+
 int main() {
-  exec("git -c status.color=always status");
+  gitnu_status("git -c status.color=always status");
+  std::string output;
+  output = shell::line("printf \"hello world\\nsecond line\"");
+  std::cout << output;
+  output = shell::line("git add");
+  std::cout << output;
   return 0;
 }
