98f3922a305be53e3b46576e8a7783366775fbfc
nguyenvukhang <brew4k@gmail.com>
2022-04-27 23:11:56 +0800

    colored git status

--- c/git.cpp
+++ c/git.cpp
@@ -20,16 +20,25 @@ void get_stdout(const char *cmd, std::promise<std::queue<std::string>> &&p) {
   p.set_value(result);
 }
 
+std::string get_filename(std::string s) { return s; }
+
 namespace git {
 void get_parallel(const char *cmd) {
   std::promise<std::queue<std::string>> p_pretty;
   std::future<std::queue<std::string>> f_pretty = p_pretty.get_future();
-  std::thread t1(&get_stdout, "git status", std::move(p_pretty));
+
+  std::promise<std::queue<std::string>> p_porcelain;
+  std::future<std::queue<std::string>> f_porcelain = p_porcelain.get_future();
+
+  std::thread t1(&get_stdout, "git -c status.color=always status", std::move(p_pretty));
+  std::thread t2(&get_stdout, "git status --porcelain", std::move(p_porcelain));
   t1.join();
-  std::queue<std::string> result = f_pretty.get();
-  while (!result.empty()) {
-    std::cout << result.front() << std::endl;
-    result.pop();
+  t2.join();
+  std::queue<std::string> pretty = f_pretty.get();
+  std::queue<std::string> porcelain = f_porcelain.get();
+  while (!pretty.empty()) {
+    std::cout << pretty.front();
+    pretty.pop();
   }
 }
 } // namespace git
