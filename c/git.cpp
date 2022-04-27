f56970edb0f27de731260226e314285ab4e9b443
nguyenvukhang <brew4k@gmail.com>
2022-04-28 00:40:53 +0800

    safe, but reading porcelain wrongly

--- c/git.cpp
+++ c/git.cpp
@@ -7,31 +7,6 @@
 #include <string>
 #include <thread>
 
-void get_stdout(const char *cmd, std::promise<std::queue<std::string>> &&p) {
-  std::queue<std::string> result;
-  std::array<char, 128> buffer;
-  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-  if (!pipe)
-    throw std::runtime_error("popen() failed!");
-  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-    result.push(buffer.data());
-  }
-  p.set_value(result);
-}
-
-void get_stdout_arr(const char *cmd, std::promise<std::array<std::string, 128>> &&p) {
-  std::array<std::string, 128> arr;
-  std::array<char, 128> buffer;
-  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-  int index = 0;
-  if (!pipe)
-    throw std::runtime_error("popen() failed!");
-  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr && index < 128) {
-    arr[index] = buffer.data();
-  }
-  p.set_value(arr);
-}
-
 void remove_newline(std::string &s) {
   s.erase(std::remove(s.begin(), s.end(), '\n'), s.end());
 }
@@ -47,7 +22,38 @@ std::string get_filename(std::string s) {
     int i = s.find(" -> ");
     return s.substr(i + 4);
   }
-  return s.substr(3);
+  if (s.length() > 3) {
+    return s.substr(3);
+  }
+  return s;
+}
+
+void get_stdout(const char *cmd, std::promise<std::queue<std::string>> &&p) {
+  std::queue<std::string> result;
+  std::array<char, 128> buffer;
+  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+  if (!pipe)
+    throw std::runtime_error("popen() failed!");
+  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+    result.push(buffer.data());
+  }
+  p.set_value(result);
+}
+
+void get_stdout_arr(const char *cmd,
+                    std::promise<std::array<std::string, 128>> &&p) {
+  std::array<std::string, 128> arr;
+  std::array<char, 128> buffer;
+  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+  int index = 0;
+  if (!pipe)
+    throw std::runtime_error("popen() failed!");
+  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr &&
+         index < 128) {
+    arr[index] = buffer.data();
+    index++;
+  }
+  p.set_value(arr);
 }
 
 // (in-place) get a string in between two substrings
@@ -74,42 +80,54 @@ std::string get_colored(std::string line) {
 
 namespace git {
 void get_parallel(const char *cmd) {
-  std::promise<std::queue<std::string>> p_pretty;
-  std::future<std::queue<std::string>> f_pretty = p_pretty.get_future();
+  // QUEUES
+  // std::promise<std::queue<std::string>> p_pretty;
+  // std::future<std::queue<std::string>> f_pretty = p_pretty.get_future();
+  //
+  // std::promise<std::queue<std::string>> p_porcelain;
+  // std::future<std::queue<std::string>> f_porcelain =
+  // p_porcelain.get_future();
 
-  std::promise<std::queue<std::string>> p_porcelain;
-  std::future<std::queue<std::string>> f_porcelain = p_porcelain.get_future();
+  // ARRAYS
+  std::promise<std::array<std::string, 128>> p_pretty;
+  std::future<std::array<std::string, 128>> f_pretty = p_pretty.get_future();
 
-  std::thread t1(&get_stdout, "git -c status.color=always status",
+  std::promise<std::array<std::string, 128>> p_porcelain;
+  std::future<std::array<std::string, 128>> f_porcelain =
+      p_porcelain.get_future();
+
+  std::thread t1(&get_stdout_arr, "git -c status.color=always status",
                  std::move(p_pretty));
-  std::thread t2(&get_stdout, "git status --porcelain", std::move(p_porcelain));
+  std::thread t2(&get_stdout_arr, "git status --porcelain",
+                 std::move(p_porcelain));
   t1.join();
   t2.join();
-  std::queue<std::string> pretty = f_pretty.get();
-  std::queue<std::string> porcelain = f_porcelain.get();
+  std::array<std::string, 128> pretty_arr = f_pretty.get();
+  std::array<std::string, 128> porcelain_arr = f_porcelain.get();
 
-  std::queue<std::string> backup;
+  for (int i = 0; i < 128; i++) {
+    porcelain_arr[i] = get_filename(porcelain_arr[i]);
+  }
 
   int index = 1;
-  while (!pretty.empty()) {
-    std::string next = get_filename(porcelain.front());
-    if (next == "") {
-      std::cout << pretty.front();
-      pretty.pop();
-      continue;
+  for (const std::string &pretty_front : pretty_arr) {
+    if (pretty_front == "") {
+      break;
     }
-    std::string colored = get_colored(pretty.front());
-    // std::cout << "(" << colored << ", " << next << ") [" <<
-    // colored.find(next) << "]" << std::endl;
-    if (colored.find(next) != std::string::npos) {
-      std::cout << index << pretty.front();
-      index++;
-      porcelain.pop();
-    } else {
-      std::cout << "(" << colored << ", " << next << ")" << std::endl;
-      std::cout << pretty.front();
+    const std::string colored = get_colored(pretty_front);
+    for (std::string &porcelain : porcelain_arr) {
+      if (porcelain == "") {
+        continue;
+      }
+      /* std::cout << "GOT HERE" << std::endl; */
+      /* std::cout << porcelain << "|" << colored << std::endl; */
+      if (colored.find(porcelain) != std::string::npos) {
+        std::cout << index << pretty_front;
+        porcelain = "";
+        index++;
+        break;
+      }
     }
-    pretty.pop();
   }
   std::cout << "COMPLETED" << std::endl;
 }
