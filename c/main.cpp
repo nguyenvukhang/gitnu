c675426caa2d3dfede196fc0ee1924df82934a91
nguyenvukhang <brew4k@gmail.com>
2022-04-28 13:02:12 +0800

    added todo

--- c/main.cpp
+++ c/main.cpp
@@ -7,5 +7,6 @@
 int main() {
   /* gitnu_status("git -c status.color=always status"); */
   git::get_parallel("git status --porcelain");
+  // TODO: handle arguments and toggle read/write accordingly
   return 0;
 }
